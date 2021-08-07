0a4cb25379f214bd103479ec8ee0a4e6eb38a148
Ruben Maher <ruben@maher.fyi>
2021-08-08 08:12:09 +0900

    fix: update for breaking changes in nixpkgs
    stdenv.lib → lib https://github.com/NixOS/nixpkgs/issues/108938
    
    Fixed in node2nix 1.9.0: https://github.com/svanderburg/node2nix/releases/tag/node2nix-1.9.0

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,7 @@
+# nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/bd807ed5dec14377f87f63c3f467adf93f03de2e.tar.gz
+
 builtins.fetchTarball {
-  name = "nixos-unstable-2020-07-23";
-  url = "https://github.com/NixOS/nixpkgs/archive/5717d9d2f7ca0662291910c52f1d7b95b568fec2.tar.gz";
-  sha256 = "17gxd2f622pyss3r6cjngdav6wzdbr31d7bqx9z2lawxg47mmk1l";
+  name = "nixos-unstable-2021-08-08";
+  url = https://github.com/NixOS/nixpkgs/archive/bd807ed5dec14377f87f63c3f467adf93f03de2e.tar.gz;
+  sha256 = "0dfpg31ikhw0acqfpdkir6pq4cjvfwnmispik3ysvy0pvgzqy75x";
 }
